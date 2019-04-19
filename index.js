3eedf96ff6d0e0a10e4a297b701fd6fa677bdf67
AUTACE <48562912+AUTACE@users.noreply.github.com>
2019-04-20 00:04:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ message.reply(  'Hier ist eine Liste der Commands: !team ==> Damit kannst du in 
 });
 
 bot.on("guildMemberAdd", function(member){
-    member.guild.channels.find("name", "╔willkommenskanal").sendMessage(member.toString() + "Willkommen auf dem Server!");
+    member.guild.channels.find("name", "neue-bewerber🚶").sendMessage(member.toString() + "Neuer Bewerber!");
 
     member.addRole(member.guild.roles.find("name", "Neu"));
 
